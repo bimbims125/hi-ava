03de15887c140a0a8a96c5bc937a84f4f8d746e9
bimbims125 <65021963+bimbims125@users.noreply.github.com>
2020-05-12 23:17:24 +0700

    upload

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
     { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "pdpb", quotes: "hohoho saya gabut" },
+    { author: "rsp", quotes: "hohoho saya gabut" },
     { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "pdpb", quotes: "Gua Gabut." },
-    { author: "-Dari Orang gabut", quotes: "maaf ya ini kegabutan gw yang kedua, semoga lu suka hehe." },
-    { author: "pdpb", quotes: "Gua Gabut." },
+    { author: "rsp", quotes: "Gua Gabut." },
+    { author: "-Dari Orang gabut", quotes: "maaf ya ini kegabutan gw, semoga lu suka hehe." },
+    { author: "rsp", quotes: "Gua Gabut." },
     { author: "standinshd", quotes: "I'm sorry for loving you. hehe." },
 ];
